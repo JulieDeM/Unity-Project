02a676a75ec8e94e19bec29dfa225c0a7bbdb575
Julie DeMasters <juliekdemasters@gmail.com>
2016-09-07 15:26:30 -0600

    added seeds, created db, created migrations, fb login working but not hooked up to the database

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
       development: {
         client: 'pg',
-        connection: 'postgres://localhost/3000'
+        connection: 'postgres://localhost/puzzletastic'
       },
       production:{
         client: 'pg',
