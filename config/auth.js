b1363b7784fd58d876f08b9aaca60619cc319dc2
Julie DeMasters <juliekdemasters@gmail.com>
2016-09-12 18:01:55 -0600

    working through passport, having troubles authenticating facebook

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,7 @@
-FB.getLoginStatus(function(response) {
-  if (response.status === 'connected') {
-    console.log('Logged in.');
+module.exports = {
+  'facebookAuth' : {
+    'clientID': '1644351665877303',
+    'clientsecret': 'b7818e92d3005a8f52260d5b18dc9450',
+    'callbackURL': "http://localhost:3000/auth/facebook/callback"
   }
-  else {
-    FB.login();
-  }
-});
+}
